07d5f01820f5f317ab3c9134a633364387de1896
aididazman <aidid.azman@microlink.com.my>
2025-04-05 03:27:09 +0800

    Initial commit - shipping-rate-service
    Initial commit - Spring Boot shipping-rate-service

---
+++ src/main/java/com/my/shippingrate/ShippingRateServiceApplication.java
@@ -0,0 +1,13 @@
+package com.my.shippingrate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShippingRateServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShippingRateServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/my/shippingrate/ShippingRateServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.shippingrate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShippingRateServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
